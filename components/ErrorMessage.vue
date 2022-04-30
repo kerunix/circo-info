55b481fab0fa8df7b80ce1bceb8f58cda9c9906e
kerunix <pierre@kissmy.co>
2022-04-30 23:03:40 +0200

    feat: added basic error handling

---
+++ components/ErrorMessage.vue
@@ -0,0 +1,25 @@
+<template> 
+<div class="rounded-md bg-red-50 p-4">
+  <div class="flex">
+    <div class="flex-shrink-0">
+      <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
+    </div>
+    <div class="ml-3">
+      <h3 class="text-sm font-medium text-red-800">{{ title }}</h3>
+      <p class="mt-2 text-sm text-red-700">
+        <slot />
+      </p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { XCircleIcon } from '@heroicons/vue/solid'
+
+interface Props {
+  title: string
+}
+
+defineProps<Props>()
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -26,51 +26,57 @@
     </form>
   </div>
   <div v-if="responses.length > 1 && !deputy" class="flex flex-col ite space-y-6 rounded p-8 bg-white md:w-8/12 text-gray-800">
-    <p class="text-center text-xl font-semibold">Veuillez affiner votre recherche en sélectionnant l'une des adresses ci dessous.</p>
-    <button
-      v-for="(address, index) in responses"
-      :key="index"
-      class="rounded bg-indigo-100 px-3 py-2 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      @click="onAddressSelected(index)"
-    >
-    {{ address.formatted }}
-    </button>
+    <ErrorMessage v-if="error?.type === 'geocoding'" :title="error.title">{{ error.message }}</ErrorMessage>
+    <template v-else>
+      <p class="text-center text-xl font-semibold">Veuillez affiner votre recherche en sélectionnant l'une des adresses ci dessous.</p>
+      <button
+        v-for="(address, index) in responses"
+        :key="index"
+        class="rounded bg-indigo-100 px-3 py-2 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        @click="onAddressSelected(index)"
+      >
+      {{ address.formatted }}
+      </button>
+    </template>
   </div>
-  <div v-if="deputy" class="flex flex-col space-y-6 rounded p-8 bg-white w-full md:w-8/12 text-gray-800">
-    <h2 class="text-center text-xl font-semibold">Votre député-e</h2>
-    <div class="mt-5 border-t border-gray-200">
-      <dl class="sm:divide-y sm:divide-gray-200">
-        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-          <dt class="text-sm font-medium text-gray-500">Nom</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.nom }}</dd>
-        </div>
-        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-          <dt class="text-sm font-medium text-gray-500">Parti</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.parti_ratt_financier }}</dd>
-        </div>
-        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-          <dt class="text-sm font-medium text-gray-500">Groupe</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.groupe_sigle }}</dd>
-        </div>
-        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-          <dt class="text-sm font-medium text-gray-500">Email</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.emails[0].email }}</dd>
-        </div>
-        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-          <dt class="text-sm font-medium text-gray-500">Profession</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.profession }}</dd>
-        </div>
-        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-          <dt class="text-sm font-medium text-gray-500">Liens</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            <div class="flex justify-start space-x-7">
-              <a class="underline" :href="deputy.url_an" target="_blank">Assemblée Nationale</a>
-              <a class="underline" :href="deputy.url_nosdeputes" target="_blank">Nos députés</a>
-            </div>
-          </dd>
-        </div>
-      </dl>
-    </div>
+  <div class="flex flex-col space-y-6 rounded p-8 bg-white w-full md:w-8/12 text-gray-800">
+    <ErrorMessage v-if="error?.type === 'circo'" :title="error.title">{{ error.message }}</ErrorMessage>
+    <template v-else>
+      <h2 class="text-center text-xl font-semibold">Votre député-e</h2>
+      <div class="mt-5 border-t border-gray-200">
+        <dl class="sm:divide-y sm:divide-gray-200">
+          <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium text-gray-500">Nom</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.nom }}</dd>
+          </div>
+          <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium text-gray-500">Parti</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.parti_ratt_financier }}</dd>
+          </div>
+          <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium text-gray-500">Groupe</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.groupe_sigle }}</dd>
+          </div>
+          <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium text-gray-500">Email</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.emails[0].email }}</dd>
+          </div>
+          <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium text-gray-500">Profession</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ deputy.profession }}</dd>
+          </div>
+          <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt class="text-sm font-medium text-gray-500">Liens</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div class="flex justify-start space-x-7">
+                <a class="underline" :href="deputy.url_an" target="_blank">Assemblée Nationale</a>
+                <a class="underline" :href="deputy.url_nosdeputes" target="_blank">Nos députés</a>
+              </div>
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </template>
   </div>
 </div>
 </template>
@@ -83,22 +89,40 @@
   const responses = ref<Record<string, any>[]>([])
   const deputy = ref<Record<string, any> | null>(null)
   const circonscription = ref<Record<string, any> | null>(null)
-  const error = ref<string | null>(null)
+  const error = ref<{ title: string, message: string, type: 'geocoding' | 'circo' } | null>(null)
 
   async function onSubmit() {
-    const res = await $fetch('/api/geocoding', { method: 'post', body: form })
-    responses.value = res.results
-    // If we only have one address matching, fetch the circo for this address immediately
-    if (res.results.length === 1) {
-      await onAddressSelected(0)
+    error.value = null
+    try {
+      const res = await $fetch('/api/geocoding', { method: 'post', body: form })
+      responses.value = res.results
+      // If we only have one address matching, fetch the circo for this address immediately
+      if (res.results.length === 1) {
+        await onAddressSelected(0)
+      }
+    } catch (err) {
+      error.value = {
+        title: 'Nous n\'avons pas réussi à localiser cette adresse.',
+        message: 'Veuillez vous assurer d\'avoir renseigné une adresse valide. Il est aussi possible que le service que nous utilisons pour localiser votre adresse soit inaccessible ou que nous ayons atteint notre quota quotidien d\'utilisations de ce service, veuillez réessayer demain.',
+        type: 'circo'
+      }      
     }
   }
 
   async function onAddressSelected(index: number) {
-    const addressPoint = Object.values(responses.value[index].geometry)
-    const payload = { coords: addressPoint }
-    const { circonscription: circo, deputy: { depute } } = await $fetch('/api/circo', { method: 'post', body: payload})
-    circonscription.value = circo
-    deputy.value = depute
+    error.value = null
+    try {
+      const addressPoint = Object.values(responses.value[index].geometry)
+      const payload = { coords: addressPoint }
+      const { circonscription: circo, deputy: { depute } } = await $fetch('/api/circo', { method: 'post', body: payload})
+      circonscription.value = circo
+      deputy.value = depute
+    } catch (err) {
+      error.value = {
+        title: 'Nous n\'avons pas trouvé votre député-e.',
+        message: 'Cela peut être dû au fait que nos données sont incomplètes ou expirées.',
+        type: 'circo'
+      }
+    }
   }
 </script>
